ab0da040915fc5f34e3b183b17db3f1501f99199
Вадим Годован <jokermind2012@yandex.ru>
2022-09-17 12:13:33 +0300

    actions_test

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! z')
+    return HttpResponse('У меня получилось! good job')
 
 
 def second_page(request):
